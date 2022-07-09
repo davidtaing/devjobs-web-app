6a4139bec3563c15fa26ad305f3f81f469f08507
David Taing <adavidtaing@gmail.com>
2022-07-09 21:53:18 +1000

    refactor: Move Header into MyApp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Header } from "../components/Header";
 import { JobList } from "../features/JobList/components/JobList";
 import { SearchBar } from "../components/SearchBar";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>devjobs - Frontend Mentor Challenge</title>
         <meta
@@ -15,13 +14,11 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
-      <Header />
-
       <main>
         <SearchBar />
         <JobList />
       </main>
-    </div>
+    </>
   );
 };
 
